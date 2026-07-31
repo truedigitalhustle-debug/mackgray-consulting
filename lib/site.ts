a5b9660b1599a8e68a393ac7d15b6619a9407320
truedigitalhustle-debug <truedigitalhustle@gmail.com>
2026-07-31 00:09:25 -0700

    Add site configuration for MackGray Consulting

---
+++ lib/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "MackGray Consulting",
+    // TODO: replace with your real contact address
+      email: "hello@mackgrayconsulting.com",
+        photo: "/images/mack-gray.jpg",
+          signatureName: "Mack Gray",
+            role: "Licensed Partner, Lacad Consulting",
+              lacadUrl: "https://www.lacadconsulting.com/",
+              };
+              
