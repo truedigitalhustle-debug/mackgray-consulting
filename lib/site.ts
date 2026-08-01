35a16a47bb9908fc8a900611335ee439b3154c6a
truedigitalhustle-debug <truedigitalhustle@gmail.com>
2026-07-31 22:29:28 -0700

    Update contact email in site configuration

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
   name: "MackGray Consulting",
-    // TODO: replace with your real contact address
-      email: "hello@mackgrayconsulting.com",
+  email: "mack@mackgrayconsulting.com",
         photo: "/images/mack-gray.jpg",
           signatureName: "Mack Gray",
             role: "Licensed Partner, Lacad Consulting",
